5cdb4d608a77f18f9e44df83a04e38da6d305ff0
cccdenhart <cccdenhart@me.com>
2020-01-04 14:26:14 -0500

    relocated dag code

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 """Executes program with user interaction."""
+
+from datetime import datetime
+
+from src.load import extract_subcomms
+from utilities import SUBR
+
+
+def main() -> None:
+    """Execute sample programs."""
+    """
+    start_date = datetime(2019, 12, 10)
+    posts = extract_subcomms(SUBR, start_date, limit=10)
+    breakpoint()
+    """
+
+
+if __name__ == "__main__":
+    main()
