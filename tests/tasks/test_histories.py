927da86aaeb718cc4bb878af84c3a180ce3e3d53
cccdenhart <cccdenhart@me.com>
2020-07-22 19:56:05 -0400

    removed collection call from hist test

--- tests/tasks/test_histories.py
+++ tests/tasks/test_histories.py
@@ -1,12 +1,11 @@
 import unittest
 
-from src.utils import DB_NAME, TEST_COLL_NAME, get_mongo, get_praw, get_psaw
+from src.utils import get_praw, get_psaw
 
 
 class TestGetUsersHistories(unittest.TestCase):
     """Tests for retrieving full posting histories for multiple users."""
 
-    coll = get_mongo()[DB_NAME][TEST_COLL_NAME]
     praw = get_praw()
     psaw = get_psaw(praw)
 
