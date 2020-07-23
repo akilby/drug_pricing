c8117fc7619536726800f7384dcdba46706cd14c
cccdenhart <cccdenhart@me.com>
2020-07-22 20:15:14 -0400

    removed api call from hist test

--- tests/tasks/test_histories.py
+++ tests/tasks/test_histories.py
@@ -1,14 +1,6 @@
 import unittest
 
-from src.utils import get_praw, get_psaw
-
 
 class TestGetUsersHistories(unittest.TestCase):
     """Tests for retrieving full posting histories for multiple users."""
 
-    praw = get_praw()
-    psaw = get_psaw(praw)
-
-
-if __name__ == '__main__':
-    unittest.main()
