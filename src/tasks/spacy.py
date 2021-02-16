9411a9edd473a046a93af545bc9bee68afcba591
cccdenhart <cccdenhart@me.com>
2021-02-16 08:46:02 -0700

    switched spacy updates to using batching

--- src/tasks/spacy.py
+++ src/tasks/spacy.py
@@ -10,13 +10,20 @@ from src.schema import Post
 
 def add_spacy_to_mongo(nlp: English) -> int:
     """Add spacy field to all posts in mongo."""
-    posts = Post.objects(Q(spacy__exists=False) & Q(text__exists=True))
-    for post in tqdm.tqdm(posts):
-        text = post.text
-        if type(text) == str:
-            post.spacy = nlp(text).to_bytes()
-            post.save()
-    return len(posts)
+    batch_size = 100
+    num_total_posts = 0
+    gen_posts = lambda: Post.objects(Q(spacy__exists=False) & Q(text__exists=True))\
+                            .limit(batch_size)
+    posts = gen_posts()
+    while len(posts) > 0:
+        num_total_posts += len(posts)
+        for post in tqdm.tqdm(posts):
+            text = post.text
+            if type(text) == str:
+                post.spacy = nlp(text).to_bytes()
+                post.save()
+        posts = gen_posts()
+    return num_total_posts
 
 
 def bytes_to_spacy(data: bytes, nlp: English) -> Doc:
