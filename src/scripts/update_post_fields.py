0ac720bcf10451af6cc0d98b73657cb8bc9eeea2
cccdenhart <cccdenhart@me.com>
2021-04-14 16:11:23 -0400

    split pids into chunks

--- src/scripts/update_post_fields.py
+++ src/scripts/update_post_fields.py
@@ -39,23 +39,38 @@ def get_posts_by_ids(is_sub: bool, ids: List[str]) -> List[Dict]:
 def fill_all_posts():
 
     # update posts without datetimes
-    print('Updating posts without datetimes .....')
+    print('Comments:')
+    print('\tRetrieving pids without datetimes .....')
     comment_subsets = Post.objects(datetime__exists=False).only('pid')
     pids = [c.pid for c in comment_subsets]
-    full_comments = get_posts_by_ids(False, pids)
+
+    print('\tRetrieving missing data from psaw .....')
+    chunk_size = 1000
+    n_chunks = int(len(pids) / chunk_size)
+    for chunk_pids in np.array_split(pids, n_chunks):
+        full_comments = get_posts_by_ids(False, chunk_pids)
     assert len(pids) == len(full_comments)
+
+    print('\tUpdating comms with missing data .....')
     for pid, real_comm in tqdm(zip(pids, full_comments)):
         comment = Comment.objects(pid=pid).first()
         comment.datetime = utc_to_dt(real_comm.created_utc)
         comment.save()
 
     # update submissions without titles
-    print('Updating submissions without titles .....')
+    print('Submissions:')
+    print('\tUpdating pids without titles/datetimes .....')
     sub_subsets = SubmissionPost.objects(Q(title__exists=False) | Q(datetime_exists=False))\
                                 .only('pid')
     pids = [s.pid for s in sub_subsets]
-    full_subs = get_posts_by_ids(True, pids)
+
+    print('\tRetrieving missing data from psaw .....')
+    n_chunks = int(len(pids) / chunk_size)
+    for chunk_pids in np.array_split(pids, n_chunks):
+        full_subs = get_posts_by_ids(True, chunk_pids)
     assert len(pids) == len(full_subs)
+
+    print('\tUpdating subs with missing data .....')
     for pid, real_sub in tqdm(zip(pids, full_subs)):
         sub = Submission.objects(pid=pid).first()
         sub.datetime = utc_to_dt(sub.created_utc)
