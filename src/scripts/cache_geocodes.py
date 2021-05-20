c85543f7c6f8ed749114be0a59d85a9ab3aa1c05
cccdenhart <cccdenhart@me.com>
2021-05-19 20:27:22 -0400

    added ent cache

--- src/scripts/cache_geocodes.py
+++ src/scripts/cache_geocodes.py
@@ -24,34 +24,52 @@ def cache_geocodes(docs: List[English], session: requests.sessions.Session, file
 
 	for e in ents:
 		if e not in ent_geocode_map:
-			ent_geocode_map[e] = forward_geocode(e, session=session, service='mapbox')
+			ent_geocode_map[e] = forward_geocode(e, session=session)
 
 	pickle.dump(ent_geocode_map, open(filepath, 'wb'))
 
 
+def cache_ents(docs: List[English], filepath: str):
+    '''Memoize a set of entities in the database.'''
+    if os.path.exists(filepath):
+        ent_list = pd.read_csv(filepath, header=None).squeeze().tolist()
+    else:
+        ent_list = []
+
+    ents = get_ents(docs, 'GPE')
+
+    for e in ents:
+        if e not in ents_list:
+            ent_list.append(e)
+
+    ent_list.to_csv(filepath, index=False, header=None)
+
+
 def main():
-	# utility variables
-	session = requests.Session()
-	cache_filename = os.path.join(ROOT_DIR, 'data', 'geocodes_cache.pk')
-	nlp = get_nlp()
-	connect_to_mongo()
+    # utility variables
+    session = requests.Session()
+    cache_filename = os.path.join(ROOT_DIR, 'data', 'geocodes_cache.pk')
+    ent_cache_filename = os.path.join(ROOT_DIR, 'data', 'ents_cache.csv')
+    nlp = get_nlp()
+    connect_to_mongo()
 
-	# retrieve all post ids
-	print('Retrieving all pids .....')
-	post_subsets = Post.objects(spacy__exists=True).only('pid').all()
-	pids = [p.pid for p in post_subsets]
+    # retrieve all post ids
+    print('Retrieving all pids .....')
+    post_subsets = Post.objects(spacy__exists=True).only('pid').all()
+    pids = [p.pid for p in post_subsets]
 
-# cache geocodes in chunks
-	print('Iterating over chunks .....')
-	chunk_size = 100000
-	pid_chunks = (pids[idx:(idx + chunk_size)]
-				  for idx in range(0, len(pids), chunk_size))
-	for pid_chunk in tqdm(pid_chunks):
-		posts = Post.objects(pid__in=pid_chunk).all()
-		docs = [bytes_to_spacy(p.spacy, nlp) for p in posts]
-		cache_geocodes(docs, session, cache_filename)
+    # cache geocodes in chunks
+    print('Iterating over chunks .....')
+    chunk_size = 100000
+    pid_chunks = (pids[idx:(idx + chunk_size)]
+                    for idx in range(0, len(pids), chunk_size))
+    for pid_chunk in tqdm(pid_chunks):
+        posts = Post.objects(pid__in=pid_chunk).all()
+        docs = [bytes_to_spacy(p.spacy, nlp) for p in posts]
+        # cache_geocodes(docs, session, cache_filename)
+        cache_ents(docs)
 
-	print('Done.')
+    print('Done.')
 
 
 if __name__ == '__main__':
