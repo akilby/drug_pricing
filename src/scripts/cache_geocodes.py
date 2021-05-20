704413d63f880e8081875494b12aa7d60c2363c7
cccdenhart <cccdenhart@me.com>
2021-05-20 11:16:42 -0400

    added cache user ents

--- src/scripts/cache_geocodes.py
+++ src/scripts/cache_geocodes.py
@@ -4,13 +4,16 @@ import requests
 
 from typing import List
 
+import tqdm
+import pandas as pd
+
 from spacy.lang.en import English
-from tqdm import tqdm
 
 from src.utils import connect_to_mongo, get_nlp, ROOT_DIR
 from src.schema import Post
 from src.tasks.spacy import bytes_to_spacy
-from src.models.__init__ import get_ents, forward_geocode
+from src.models.__init__ import get_user_spacy, get_ents, DENYLIST, forward_geocode
+from src.models.filters import BaseFilter, DenylistFilter, LocationFilter
 
 
 def cache_geocodes(docs: List[English], session: requests.sessions.Session, filepath: str):
@@ -39,10 +42,42 @@ def cache_ents(docs: List[English], filepath: str):
     ents = get_ents(docs, 'GPE')
 
     for e in ents:
-        if e not in ents_list:
+        if e not in ent_list:
             ent_list.append(e)
 
-    ent_list.to_csv(filepath, index=False, header=None)
+    pd.Series(ent_list).to_csv(filepath, index=False, header=None)
+
+
+def cache_user_ents(user: User, filepath: str, lc: LocationClusterer):
+    '''Cache all entities for a given user.'''
+    if os.path.exists(filepath):
+        user_ents_cache = pickle.load(open(filepath, 'rb'))
+    else:
+        user_ents_cache = {}
+
+    user_ents = lc.extract_entities(user)
+
+    if user.username not in user_ents_cache:
+        user_ents_cache[user.username] = user_ents
+
+        pickle.dump(user_ents_cache, open(filepath, 'wb'))
+
+
+def cache_all_user_ents():
+    print('Initializing .....')
+    cache_filepath = os.path.join(ROOT_DIR, 'data', 'user_ents_cache.pk')
+    nlp = get_nlp()
+    connect_to_mongo()
+    gazetteer = pd.read_csv("data/locations/grouped-locations.csv")
+    filters = [DenylistFilter(DENYLIST), LocationFilter(gazetteer)]
+    model = LocationClusterer(filters, nlp)
+    all_users = Users.objects.all()
+
+    print('Caching user entities .....')
+    for user in tqdm.tqdm(all_users):
+        cache_user_ents(user, cache_filepath, model)
+
+    print('Done.')
 
 
 def main():
@@ -63,14 +98,15 @@ def main():
     chunk_size = 100000
     pid_chunks = (pids[idx:(idx + chunk_size)]
                     for idx in range(0, len(pids), chunk_size))
-    for pid_chunk in tqdm(pid_chunks):
+    for pid_chunk in tqdm.tqdm(pid_chunks):
         posts = Post.objects(pid__in=pid_chunk).all()
         docs = [bytes_to_spacy(p.spacy, nlp) for p in posts]
         # cache_geocodes(docs, session, cache_filename)
-        cache_ents(docs)
+        cache_ents(docs, ent_cache_filename)
 
     print('Done.')
 
 
 if __name__ == '__main__':
-	main()
+	# main()
+    cache_all_user_ents()
